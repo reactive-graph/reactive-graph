301f63a9953f2cf084412caf8e674bfaca458d02
Andreas Schaeffer <andreas@schaeffernet.de>
2021-08-30 19:54:49 +0200

    Added more string generation methods

--- src/tests/utils/mod.rs
+++ src/tests/utils/mod.rs
@@ -7,8 +7,15 @@ use serde_json::{Value, json};
 const CHARSET_LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 pub fn r_string() -> String {
-    // let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     generate(10, CHARSET_LETTERS).to_string()
+}
+
+pub fn r_string_255() -> String {
+    generate(255, CHARSET_LETTERS).to_string()
+}
+
+pub fn r_string_1000() -> String {
+    generate(1000, CHARSET_LETTERS).to_string()
 }
 
 pub fn r_json_string() -> Value {
