366d20f25df7c7159c4bad69bec9b4011ffb55ba
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-04 17:35:54 +0200

    Added code documentation

--- src/entity_behaviour_provider.rs
+++ src/entity_behaviour_provider.rs
@@ -4,9 +4,12 @@ use inexor_rgf_core_model::ReactiveEntityInstance;
 use uuid::Uuid;
 
 pub trait EntityBehaviourProvider: Send + Sync {
+    /// Possibly adds new behaviour to the given entity instance
     fn add_behaviours(&self, entity_instance: Arc<ReactiveEntityInstance>);
 
+    /// Removes behaviour to the given entity instance
     fn remove_behaviours(&self, entity_instance: Arc<ReactiveEntityInstance>);
 
+    /// Removes behaviour to the given entity instance by uuid
     fn remove_behaviours_by_id(&self, id: Uuid);
 }
