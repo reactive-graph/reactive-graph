069449234676a5a0496352407e86a75215bb662c
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-05 13:52:40 +0200

    Experiment with two libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![feature(unsized_tuple_coercion)]
+#![feature(in_band_lifetimes)]
+#![feature(concat_idents)]
+
 use crate::plugin::registry::PluginRegistry;
 pub use inexor_rgf_core_plugins as plugins;
 use std::alloc::System;
@@ -21,10 +25,18 @@ fn main() {
         registry
             .load("/home/aschaeffer/CLionProjects/inexor-rgf-plugin-base/target/debug/libinexor_rgf_plugin_base.so")
             .expect("Failed to load BASE plugin");
+        registry
+            .load("/home/aschaeffer/CLionProjects/inexor-rgf-plugin-metadata/target/debug/libinexor_rgf_plugin_metadata.so")
+            .expect("Failed to load METADATA plugin");
 
+        registry.list();
         registry.init("base");
+        registry.init("metadata");
         registry.post_init("base");
+        registry.post_init("metadata");
+        registry.pre_shutdown("metadata");
         registry.pre_shutdown("base");
+        registry.shutdown("metadata");
         registry.shutdown("base");
     }
 }
