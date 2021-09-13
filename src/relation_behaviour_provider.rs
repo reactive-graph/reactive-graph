8104e920380c80c9b88eaddfed2c6655b6fcb03f
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-13 22:26:32 +0200

    Added interface documentation

--- src/relation_behaviour_provider.rs
+++ src/relation_behaviour_provider.rs
@@ -4,9 +4,12 @@ use indradb::EdgeKey;
 use inexor_rgf_core_model::ReactiveRelationInstance;
 
 pub trait RelationBehaviourProvider: Send + Sync {
+    /// Possibly adds new behaviour to the given relation instance
     fn add_behaviours(&self, relation_instance: Arc<ReactiveRelationInstance>);
 
+    /// Removes behaviour from the given relation instance
     fn remove_behaviours(&self, relation_instance: Arc<ReactiveRelationInstance>);
 
+    /// Removes behaviour from the given relation instance by edge key
     fn remove_behaviours_by_key(&self, edge_key: EdgeKey);
 }
