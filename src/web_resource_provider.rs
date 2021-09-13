447ec12d81e90df5dfca0eb526ea24231dbedb52
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-13 02:36:59 +0200

    Try other method

--- src/web_resource_provider.rs
+++ src/web_resource_provider.rs
@@ -1,5 +1,6 @@
-use actix_web_static_files::ResourceFiles;
+use actix_web_static_files::deps::static_files::Resource;
+use std::collections::HashMap;
 
 pub trait WebResourceProvider: Send + Sync {
-    fn get_web_resources(&self) -> ResourceFiles;
+    fn get_web_resources(&self) -> HashMap<&'static str, Resource>;
 }
