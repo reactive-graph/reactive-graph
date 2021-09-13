dc81580953c59113e4f8d2c99e4a3557b460a3fa
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-13 23:37:22 +0200

    Cleanup

--- src/implementation/relation_behaviour_manager_impl.rs
+++ src/implementation/relation_behaviour_manager_impl.rs
@@ -6,7 +6,6 @@ use waiter_di::*;
 use crate::api::RelationBehaviourManager;
 use crate::model::ReactiveRelationInstance;
 use crate::plugins::RelationBehaviourProvider;
-// use crate::registry::relation::*;
 use indradb::EdgeKey;
 use log::debug;
 
@@ -22,8 +21,6 @@ fn create_relation_behaviour_providers() -> RelationBehaviourProviders {
 
 #[component]
 pub struct RelationBehaviourManagerImpl {
-    // TODO: migrate the providers to subsystems
-    // connectors_registry: Wrc<dyn ConnectorsRegistry>,
     behaviour_providers: RelationBehaviourProviders,
 }
 
@@ -31,37 +28,23 @@ pub struct RelationBehaviourManagerImpl {
 #[provides]
 impl RelationBehaviourManager for RelationBehaviourManagerImpl {
     fn add_behaviours(&self, relation_instance: Arc<ReactiveRelationInstance>) {
-        // TODO: migrate all registries into subsystems
-        // TODO: safety checks
         debug!(
             "RelationBehaviourManager::add_behaviours {}",
             relation_instance.get_key().unwrap().t.0.as_str()
         );
-        // self.connectors_registry
-        //     .add_behaviours(relation_instance.clone());
-
         // TODO: unit test with multiple behaviours on a single relation
-
         for provider in self.behaviour_providers.0.read().unwrap().iter() {
             provider.add_behaviours(relation_instance.clone())
         }
     }
 
     fn remove_behaviours(&self, relation_instance: Arc<ReactiveRelationInstance>) {
-        // TODO: migrate all registries into subsystems
-        // self.connectors_registry
-        //     .remove_behaviours(relation_instance.clone());
-
         for provider in self.behaviour_providers.0.read().unwrap().iter() {
             provider.remove_behaviours(relation_instance.clone())
         }
     }
 
     fn remove_behaviours_by_key(&self, edge_key: EdgeKey) {
-        // TODO: migrate all registries into subsystems
-        // self.connectors_registry
-        //     .remove_behaviours_by_key(edge_key.clone());
-
         for provider in self.behaviour_providers.0.read().unwrap().iter() {
             provider.remove_behaviours_by_key(edge_key.clone())
         }
