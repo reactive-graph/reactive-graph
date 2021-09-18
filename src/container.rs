f6d1412435edfa870a789a0ad3c728b769b60c85
Andreas Schaeffer <andreas@schaeffernet.de>
2021-09-18 23:37:54 +0200

    Disable parse_args because of unchecked unwraps

--- src/container.rs
+++ src/container.rs
@@ -1,12 +1,12 @@
-use std::any::{TypeId, type_name};
+use crate::{RcAny, Wrc};
+use config::{Config, Environment, File};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::any::{type_name, TypeId};
 use std::collections::HashMap;
 use std::env;
-use std::marker::PhantomData;
-use config::{Config, Environment, File};
-use regex::Regex;
 use std::env::args;
-use lazy_static::lazy_static;
-use crate::{RcAny, Wrc};
+use std::marker::PhantomData;
 
 pub mod profiles {
     pub struct Default;
@@ -27,11 +27,7 @@ pub trait Provider<T: ?Sized> {
     fn get_ref(&mut self) -> &Self::Impl {
         // Value under RC is still stored in container, so it can be safely returned as a reference
         // that has the same life as container reference
-        unsafe {
-            Wrc::as_ptr(&Self::get(self))
-                .as_ref()
-                .unwrap()
-        }
+        unsafe { Wrc::as_ptr(&Self::get(self)).as_ref().unwrap() }
     }
     fn create_boxed(&mut self) -> Box<Self::Impl> {
         Box::new(Self::create(self))
@@ -41,35 +37,37 @@ pub trait Provider<T: ?Sized> {
 pub struct Container<P> {
     profile: PhantomData<P>,
     pub config: Config,
-    pub components: HashMap<TypeId, RcAny>
+    pub components: HashMap<TypeId, RcAny>,
 }
 
 impl<P> Container<P> {
     pub fn new() -> Container<P> {
         let mut config = Config::new();
-        config.merge(File::with_name("config/default").required(false))
+        config
+            .merge(File::with_name("config/default").required(false))
             .expect("Failed to read default config file");
 
         let profile = profile_name::<P>();
         if profile.ne(&"default".to_string()) {
-            config.merge(File::with_name(&format!("config/{}", profile)).required(false))
+            config
+                .merge(File::with_name(&format!("config/{}", profile)).required(false))
                 .expect(format!("Failed to read {} config file", profile).as_str());
         }
 
-        config.merge(Environment::new())
+        config
+            .merge(Environment::new())
             .expect("Failed to load environment");
 
-        config.merge(parse_args())
-            .expect("Failed to parse args");
+        // config.merge(parse_args())
+        //     .expect("Failed to parse args");
 
         Container {
             config,
             profile: PhantomData::<P>,
-            components: HashMap::new()
+            components: HashMap::new(),
         }
     }
 }
-
 
 lazy_static! {
     pub static ref APP_PROFILE: String = parse_profile();
@@ -78,10 +76,12 @@ lazy_static! {
 fn parse_profile() -> String {
     let mut config = Config::new();
 
-    config.merge(File::with_name("config/default").required(false))
+    config
+        .merge(File::with_name("config/default").required(false))
         .expect("Failed to read default config file");
 
-    let profile_arg = args().position(|arg| arg.as_str() == "--profile")
+    let profile_arg = args()
+        .position(|arg| arg.as_str() == "--profile")
         .and_then(|arg_pos| args().nth(arg_pos + 1));
 
     let parsed_profile = profile_arg
@@ -107,6 +107,7 @@ pub fn parse_args() -> Config {
                 if value.is_none() || value.unwrap().starts_with("--") {
                     config.set(&arg[2..], true).unwrap();
                 } else {
+                    let arg = args.next().unwrap();
                     config.set(&arg[2..], args.next().unwrap()).unwrap();
                 }
             }
@@ -121,7 +122,8 @@ pub fn parse_args() -> Config {
 pub fn profile_name<T>() -> String {
     let profile_type_name = type_name::<T>().to_lowercase();
 
-    Regex::new(r".*::").unwrap()
+    Regex::new(r".*::")
+        .unwrap()
         .replace(profile_type_name.as_str(), "")
         .to_string()
 }
