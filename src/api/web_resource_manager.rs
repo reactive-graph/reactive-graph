a986539f83adc40903a21f66fee0a20085ed2ef5
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-06 23:39:46 +0100

    Map http request and response to actix web request and response (Closes #8)
    This makes the plugin api a lot smaller.

--- src/api/web_resource_manager.rs
+++ src/api/web_resource_manager.rs
@@ -7,7 +7,7 @@ use crate::plugins::WebResourceProvider;
 
 #[async_trait]
 pub trait WebResourceManager: Send + Sync + Lifecycle {
-    fn has(&self, web_resource_name: String) -> bool;
-    fn get(&self, web_resource_name: String) -> Option<Arc<dyn WebResourceProvider>>;
+    fn has(&self, base_path: String) -> bool;
+    fn get(&self, base_path: String) -> Option<Arc<dyn WebResourceProvider>>;
     fn add_provider(&self, web_resource_provider: Arc<dyn WebResourceProvider>);
 }
