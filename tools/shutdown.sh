3e6a6d36effd37deca46821800231d3cebb9622b
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-06 02:08:44 +0100

    Trigger shutdown with delay

--- tools/shutdown.sh
+++ tools/shutdown.sh
@@ -2,5 +2,5 @@
 curl -g \
   -X POST \
   -H "Content-Type: application/json" \
-  -d '{"query": "mutation { instances { entities { update(label: \"/org/inexor/system/shutdown\", properties: [ { name: \"shutdown\", value: true } ] ) { id } } } }" }' \
+  -d '{"query": "mutation { instances { entities { update(label: \"/org/inexor/system/shutdown\", properties: [ { name: \"shutdown\", value: 3 } ] ) { id } } } }" }' \
   http://localhost:31415/graphql
