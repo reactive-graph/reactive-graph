acae720a4b59a60b10b9f9a712eecf58e1151245
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-06 18:57:18 +0100

    Rename response body to http body + Added parameter request in handle_web_resource

--- src/web_resource_provider.rs
+++ src/web_resource_provider.rs
@@ -1,17 +1,10 @@
+use crate::HttpBody;
 use http::{Request, Response, Result};
-use serde_json::Value;
-
-enum ResponseBody {
-    None,
-    Binary(Vec<u8>),
-    Json(Value),
-    PlainText(String),
-}
 
 pub trait WebResourceProvider: Send + Sync {
     /// The base path segment.
     fn get_base_path(&self) -> String;
 
-    /// The base path segment.
-    fn handle_web_resource(&self, path: String) -> Result<Response<ResponseBody>>;
+    /// Handles a web resource.
+    fn handle_web_resource(&self, path: String, request: Request<HttpBody>) -> Result<Response<HttpBody>>;
 }
