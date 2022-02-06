c9a74eba21941540fffa4452567e470105317b39
Andreas Schaeffer <andreas@schaeffernet.de>
2022-02-06 18:49:47 +0100

    Replace actix-web with much smaller http crate for crating and returning HTTP responses

--- src/web_resource_provider.rs
+++ src/web_resource_provider.rs
@@ -1,7 +1,17 @@
-use actix_web::HttpResponse;
+use http::{Request, Response, Result};
+use serde_json::Value;
+
+enum ResponseBody {
+    None,
+    Binary(Vec<u8>),
+    Json(Value),
+    PlainText(String),
+}
 
 pub trait WebResourceProvider: Send + Sync {
-    fn get_name(&self) -> String;
+    /// The base path segment.
+    fn get_base_path(&self) -> String;
 
-    fn handle_web_resource(&self, path: String) -> HttpResponse;
+    /// The base path segment.
+    fn handle_web_resource(&self, path: String) -> Result<Response<ResponseBody>>;
 }
