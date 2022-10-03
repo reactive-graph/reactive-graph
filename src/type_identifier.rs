8fd303857152fdfa48304ecb60e9550235245c67
Andreas Schaeffer <andreas@schaeffernet.de>
2022-10-04 01:09:17 +0200

    Added reverse method for getting the namespace and type name from an type identifier

--- src/type_identifier.rs
+++ src/type_identifier.rs
@@ -10,3 +10,12 @@ pub fn fully_qualified_identifier(namespace: &str, name: &str, type_namespace: &
     Identifier::new(fully_qualified_name.as_str())
         .unwrap_or_else(|_| Identifier::new(Uuid::new_v5(type_namespace, fully_qualified_name.as_bytes()).to_string()).unwrap())
 }
+
+/// Returns the namespace and type name from the given identifier.
+pub fn get_namespace_and_type_name(t: Identifier) -> (String, String) {
+    let type_name = t.to_string();
+    match type_name.split_once("__") {
+        Some((namespace, type_name)) => (namespace.to_string(), type_name.to_string()),
+        None => (String::new(), type_name),
+    }
+}
