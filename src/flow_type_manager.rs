f1591729d8ed009bf695862dddec9cefc9aa53c9
Andreas Schaeffer <andreas@schaeffernet.de>
2022-10-20 21:25:11 +0200

    Added flow type modification methods

--- src/flow_type_manager.rs
+++ src/flow_type_manager.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::model::EntityInstance;
 use crate::model::Extension;
 use crate::model::FlowType;
@@ -43,6 +45,33 @@ pub trait FlowTypeManager: Send + Sync {
         variables: Vec<PropertyType>,
         extensions: Vec<Extension>,
     );
+
+    /// Adds the given entity instance to the flow type with the given name.
+    fn add_entity_instance(&self, name: &str, entity_instance: EntityInstance);
+
+    /// Updates the entity instance with the given id of the flow type with the given name.
+    fn update_entity_instance(&self, name: &str, id: Uuid, entity_instance: EntityInstance);
+
+    /// Removes the entity instance with the given id from the flow type with the given name.
+    fn remove_entity_instance(&self, name: &str, id: Uuid);
+
+    /// Adds the given extension to the flow type with the given name.
+    fn add_extension(&self, name: &str, extension: Extension);
+
+    /// Updates the extension with the given name of the flow type with the given name.
+    fn update_extension(&self, flow_name: &str, extension_name: &str, extension: Extension);
+
+    /// Removes the extension with the given name from the flow type with the given name.
+    fn remove_extension(&self, flow_name: &str, extension_name: &str);
+
+    /// Adds the given variable to the flow type with the given name.
+    fn add_variable(&self, name: &str, variable: PropertyType);
+
+    /// Updates the variable with the given name of the flow type with the given name.
+    fn update_variable(&self, flow_name: &str, variable_name: &str, variable: PropertyType);
+
+    /// Removes the variable with the given name from the flow type with the given name.
+    fn remove_variable(&self, flow_name: &str, variable_name: &str);
 
     /// Deletes the flow type with the given name.
     fn delete(&self, name: &str);
