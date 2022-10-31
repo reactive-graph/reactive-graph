6c857256ec27824a6f95e8b1e35ce850b6730750
Andreas Schaeffer <andreas@schaeffernet.de>
2022-10-31 21:27:55 +0100

    Remove unnecessary DAO type

--- src/lib.rs
+++ src/lib.rs
@@ -108,7 +108,7 @@ macro_rules! export_plugin {
 
 #[macro_export]
 macro_rules! embedded_asset_provider_impl {
-    ($asset: ident, $dao: ident, $ty: ident) => {{
+    ($asset: ident, $ty: ident) => {{
         let mut entries = Vec::new();
         for file in $asset::iter() {
             let filename = file.as_ref();
@@ -117,8 +117,8 @@ macro_rules! embedded_asset_provider_impl {
                 Some(asset) => match std::str::from_utf8(asset.data.as_ref()) {
                     Ok(json_str) => match serde_json::from_str(json_str) {
                         Ok(parsed_entry) => {
-                            let dao_entry: $dao = parsed_entry;
-                            entries.push($ty::from(&dao_entry));
+                            let entry: $ty = parsed_entry;
+                            entries.push(entry);
                         }
                         Err(e) => error!("Error in parsing JSON file {}: {}", filename, e),
                     },
