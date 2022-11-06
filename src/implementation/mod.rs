9f4ea3bb4f32c9670b6edc651c484f3fe943d4e9
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:58:41 +0100

    Refactored reactive flow instance manager for type identifiers

--- src/implementation/mod.rs
+++ src/implementation/mod.rs
@@ -14,7 +14,7 @@ pub use graphql_schema_manager_impl::*;
 pub use graphql_server_impl::*;
 pub use plugin_registry_impl::*;
 pub use reactive_entity_instance_manager_impl::*;
-pub use reactive_flow_manager_impl::*;
+pub use reactive_flow_instance_manager_impl::*;
 pub use reactive_relation_instance_manager_impl::*;
 pub use relation_behaviour_manager_impl::*;
 pub use relation_edge_manager_impl::*;
@@ -39,13 +39,29 @@ pub mod graphql_schema_manager_impl;
 pub mod graphql_server_impl;
 pub mod plugin_registry_impl;
 pub mod reactive_entity_instance_manager_impl;
-pub mod reactive_flow_manager_impl;
+pub mod reactive_flow_instance_manager_impl;
 pub mod reactive_relation_instance_manager_impl;
+pub mod reactive_type_identifiers;
 pub mod relation_behaviour_manager_impl;
 pub mod relation_edge_manager_impl;
 pub mod relation_instance_manager_impl;
 pub mod relation_type_manager_impl;
 pub mod shutdown_manager_impl;
 pub mod web_resource_manager_impl;
-pub mod reactive_type_identifiers;
 
+const NAMESPACE_CORE: &str = "core";
+const NAMESPACE_LOGICAL: &str = "logical";
+const NAMESPACE_FLOW: &str = "flow";
+
+const TYPE_SYSTEM_EVENT: &str = "system_event";
+const TYPE_GENERIC_FLOW: &str = "generic_flow";
+const TYPE_SHUTDOWN: &str = "shutdown";
+
+const COMPONENT_LABELED: &str = "labeled";
+const COMPONENT_ACTION: &str = "action";
+const COMPONENT_EVENT: &str = "event";
+
+const PROPERTY_LABEL: &str = "label";
+const PROPERTY_SHUTDOWN: &str = "shutdown";
+const PROPERTY_TRIGGER: &str = "trigger";
+const PROPERTY_EVENT: &str = "event";
