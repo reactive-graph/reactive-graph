0a36e62a707b1005617714581ba69d31b8af1b37
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:29:31 +0100

    Refactored relation type REST API for type identifiers

--- src/rest/types/relations.rs
+++ src/rest/types/relations.rs
@@ -1,6 +1,8 @@
 use std::sync::Arc;
 
-use actix_web::{get, web, HttpResponse};
+use actix_web::get;
+use actix_web::web;
+use actix_web::HttpResponse;
 use mime::APPLICATION_JSON;
 
 use crate::api::RelationTypeManager;
@@ -9,18 +11,21 @@ use crate::api::RelationTypeManager;
 pub async fn get_relation_types(relation_type_manager: web::Data<Arc<dyn RelationTypeManager>>) -> HttpResponse {
     HttpResponse::Ok()
         .content_type(APPLICATION_JSON.to_string())
-        .json(relation_type_manager.get_relation_types())
+        .json(relation_type_manager.get_all())
 }
 
-#[get("/types/relations/{name}")]
-pub async fn get_relation_type(name: web::Path<(String,)>, relation_type_manager: web::Data<Arc<dyn RelationTypeManager>>) -> HttpResponse {
-    let name = name.into_inner().0;
-    let relation_type = relation_type_manager.get(&name);
-    if relation_type.is_some() {
-        HttpResponse::Ok().content_type(APPLICATION_JSON.to_string()).json(relation_type)
-    } else {
-        HttpResponse::NotFound()
+#[get("/types/relations/{namespace}/{type_name}")]
+pub async fn get_relation_type(
+    namespace: web::Path<(String,)>,
+    type_name: web::Path<(String,)>,
+    relation_type_manager: web::Data<Arc<dyn RelationTypeManager>>,
+) -> HttpResponse {
+    let namespace = namespace.into_inner().0;
+    let type_name = type_name.into_inner().0;
+    match relation_type_manager.get_by_type(&namespace, &type_name) {
+        Some(relation_type) => HttpResponse::Ok().content_type(APPLICATION_JSON.to_string()).json(&relation_type),
+        None => HttpResponse::NotFound()
             .content_type(APPLICATION_JSON.to_string())
-            .body(format!("Relation Type {} not found", name))
+            .body(format!("Relation Type {}__{} not found", namespace, type_name)),
     }
 }
