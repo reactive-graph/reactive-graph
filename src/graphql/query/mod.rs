15b008088b669610350f3035f8474b702399a017
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:39:31 +0100

    Removed temporary debugging endpoint for the dynamic graph feature

--- src/graphql/query/mod.rs
+++ src/graphql/query/mod.rs
@@ -1,14 +1,9 @@
-use std::sync::Arc;
-
 use async_graphql::*;
-use log::error;
-use log::info;
 use uuid::Uuid;
 
 pub use instances::*;
 pub use types::*;
 
-use crate::api::DynamicGraph;
 use crate::graphql::query::Instances;
 use crate::graphql::query::Types;
 
@@ -32,16 +27,5 @@ impl InexorQuery {
 
     async fn random_uuid(&self, _context: &Context<'_>) -> String {
         Uuid::new_v4().to_string()
-    }
-
-    async fn dynamic_graph(&self, context: &Context<'_>) -> String {
-        let dynamic_graph = context.data::<Arc<dyn DynamicGraph>>();
-        if dynamic_graph.is_err() {
-            error!("error: {:?}", dynamic_graph.err().unwrap());
-            return "{}".to_owned();
-        }
-        let sdl = dynamic_graph.unwrap().create_sdl();
-        info!("Schema\n\n{}\n\n", sdl.to_string());
-        sdl.to_string()
     }
 }
