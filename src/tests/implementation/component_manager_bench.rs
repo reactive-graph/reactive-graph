582574373e8fdc556a5c3df44138dea935ff9367
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:11:28 +0100

    Updated unit tests

--- src/tests/implementation/component_manager_bench.rs
+++ src/tests/implementation/component_manager_bench.rs
@@ -4,6 +4,7 @@ use std::process::Termination;
 use test::Bencher;
 
 use crate::builder::ComponentBuilder;
+use crate::model::ComponentTypeId;
 use crate::tests::utils::application::init_application;
 use crate::tests::utils::r_string;
 
@@ -14,11 +15,10 @@ fn creation_benchmark(bencher: &mut Bencher) -> impl Termination {
     let namespace = r_string();
     let component_name = r_string();
     let property_name = r_string();
-    let component = ComponentBuilder::new(namespace.as_str(), component_name.as_str())
-        .string_property(property_name.as_str())
-        .build();
+    let ty = ComponentTypeId::new_from_type(namespace.clone(), component_name.clone());
+    let component = ComponentBuilder::new(ty.clone()).string_property(&property_name).build();
     bencher.iter(move || {
-        component_manager.register(component.clone());
-        component_manager.delete(component_name.as_str());
+        let _ = component_manager.register(component.clone());
+        component_manager.delete(&ty);
     })
 }
--- src/tests/implementation/component_manager_test.rs
+++ src/tests/implementation/component_manager_test.rs
@@ -3,8 +3,9 @@ use std::env;
 use serde_json::json;
 
 use crate::model::Component;
-use crate::model::DataType;
+use crate::model::ComponentTypeId;
 use crate::model::Extension;
+use crate::model::NamespacedTypeGetter;
 use crate::model::PropertyType;
 use crate::tests::utils::application::init_application;
 use crate::tests::utils::r_string;
@@ -18,17 +19,19 @@ fn test_register_component() {
     let description = r_string();
     let property_name = r_string();
     let extension_name = r_string();
-    component_manager.register(Component::new(
-        namespace.clone(),
-        component_name.clone(),
-        description.clone(),
-        vec![PropertyType::new(property_name.clone(), DataType::String)],
+    let ty = ComponentTypeId::new_from_type(namespace.clone(), component_name.clone());
+    let component = Component::new(
+        &ty,
+        &description,
+        vec![PropertyType::string(&property_name)],
         vec![Extension::new(extension_name.clone(), json!(""))],
-    ));
-    assert!(component_manager.has(component_name.as_str()));
+    );
+    assert!(component_manager.register(component).is_ok());
+    assert!(component_manager.has(&ty));
 
-    let component = component_manager.get(component_name.as_str()).unwrap();
-    assert_eq!(component_name, component.name);
+    let component = component_manager.get(&ty).unwrap();
+    assert_eq!(namespace, component.namespace());
+    assert_eq!(component_name, component.type_name());
     assert!(component.has_property(property_name.clone()));
     assert!(!component.has_property(r_string()));
 }
@@ -37,11 +40,11 @@ fn test_register_component() {
 fn test_get_components() {
     let application = init_application();
     let component_manager = application.get_component_manager();
-    let components = component_manager.get_components();
+    let components = component_manager.get_all();
     for component in components {
-        assert!(component_manager.has(component.name.as_str()));
+        assert!(component_manager.has(&component.ty));
     }
-    assert!(!component_manager.has(r_string().as_str()));
+    assert!(!component_manager.has(&ComponentTypeId::new_from_type(r_string(), r_string())));
 }
 
 #[test]
@@ -58,17 +61,19 @@ fn test_export_import_component() {
     path.push(format!("{}.json", component_name));
     let path = path.into_os_string().into_string().unwrap();
 
-    component_manager.create(
-        namespace.as_str(),
-        component_name.as_str(),
-        description.as_str(),
-        vec![PropertyType::new(property_name.as_str(), DataType::String)],
+    let ty = ComponentTypeId::new_from_type(&namespace, &component_name);
+    let result = component_manager.create(
+        &ty,
+        &description,
+        vec![PropertyType::string(&property_name)],
         vec![Extension::new(extension_name.as_str(), json!(""))],
     );
-    component_manager.export(component_name.as_str(), path.as_str());
-    assert!(component_manager.has(component_name.as_str()));
-    component_manager.delete(component_name.as_str());
-    assert!(!component_manager.has(component_name.as_str()));
-    component_manager.import(path.as_str());
-    assert!(component_manager.has(component_name.as_str()));
+    assert!(result.is_ok());
+    component_manager.export(&ty, path.as_str());
+    assert!(component_manager.has(&ty));
+    component_manager.delete(&ty);
+    assert!(!component_manager.has(&ty));
+    let result = component_manager.import(path.as_str());
+    assert!(result.is_ok());
+    assert!(component_manager.has(&ty));
 }
