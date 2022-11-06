ce1d7c140b17898eb1d6ff9a703c3d5239cee407
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:38:37 +0100

    Refactored GraphQL API type system mutation for type identifiers

--- src/graphql/mutation/types/mod.rs
+++ src/graphql/mutation/types/mod.rs
@@ -1,13 +1,25 @@
+pub use behaviour_type_id::*;
 pub use component::*;
+pub use component_type_id::*;
 pub use entity_type::*;
+pub use entity_type_id::*;
+// pub use extension::*;
 pub use flow_type::*;
+pub use flow_type_id::*;
 pub use property_type::*;
 pub use relation_type::*;
+pub use relation_type_id::*;
 pub use types::*;
 
+pub mod behaviour_type_id;
 pub mod component;
+pub mod component_type_id;
 pub mod entity_type;
+pub mod entity_type_id;
+// pub mod extension;
 pub mod flow_type;
+pub mod flow_type_id;
 pub mod property_type;
 pub mod relation_type;
+pub mod relation_type_id;
 pub mod types;
