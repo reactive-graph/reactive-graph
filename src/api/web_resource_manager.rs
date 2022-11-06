aa58cb6f70356a7eb0e6c8e007923648a98d9c6e
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-06 01:18:39 +0100

    Make web resource providers removable + Use DashMap

--- src/api/web_resource_manager.rs
+++ src/api/web_resource_manager.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use uuid::Uuid;
 
 use crate::api::Lifecycle;
 use crate::plugins::WebResourceProvider;
@@ -11,5 +12,10 @@ pub trait WebResourceManager: Send + Sync + Lifecycle {
     fn get(&self, base_path: String) -> Option<Arc<dyn WebResourceProvider>>;
     fn get_default(&self) -> Option<Arc<dyn WebResourceProvider>>;
     fn get_default_base_path(&self) -> Option<String>;
-    fn add_provider(&self, web_resource_provider: Arc<dyn WebResourceProvider>);
+
+    /// Registers a web resource provider.
+    fn add_provider(&self, id: Uuid, web_resource_provider: Arc<dyn WebResourceProvider>);
+
+    /// Unregisters a web resource provider.
+    fn remove_provider(&self, id: &Uuid);
 }
--- src/implementation/web_resource_manager_impl.rs
+++ src/implementation/web_resource_manager_impl.rs
@@ -1,20 +1,28 @@
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
+use std::sync::RwLock;
 
-use crate::di::{component, provides, wrapper};
+use crate::di::component;
+use crate::di::provides;
+use crate::di::wrapper;
 use async_trait::async_trait;
+use dashmap::DashMap;
 use log::debug;
+use uuid::Uuid;
 
 use crate::api::Lifecycle;
 use crate::api::WebResourceManager;
 use crate::plugins::WebResourceProvider;
 
 #[wrapper]
-pub struct WebResourceProviders(RwLock<HashMap<String, Arc<dyn WebResourceProvider>>>);
+pub struct WebResourceProviders(DashMap<String, Arc<dyn WebResourceProvider>>);
+
+#[wrapper]
+pub struct WebResourceProviderPaths(DashMap<Uuid, String>);
 
 pub struct WebResourceManagerImpl {
     default_base_path: RwLock<Option<String>>,
     web_resource_providers: WebResourceProviders,
+    web_resource_provider_paths: WebResourceProviderPaths,
 }
 
 #[component]
@@ -23,7 +31,8 @@ impl WebResourceManagerImpl {
     fn new() -> Self {
         Self {
             default_base_path: RwLock::new(None),
-            web_resource_providers: WebResourceProviders(RwLock::new(HashMap::new())),
+            web_resource_providers: WebResourceProviders(DashMap::new()),
+            web_resource_provider_paths: WebResourceProviderPaths(DashMap::new()),
         }
     }
 }
@@ -36,22 +45,30 @@ impl WebResourceManager for WebResourceManagerImpl {
     }
 
     fn get(&self, base_path: String) -> Option<Arc<dyn WebResourceProvider>> {
-        self.web_resource_providers.0.read().unwrap().get(base_path.as_str()).cloned()
+        self.web_resource_providers.0.get(base_path.as_str()).map(|p| p.value().clone())
     }
 
     fn get_default(&self) -> Option<Arc<dyn WebResourceProvider>> {
         self.get_default_base_path()
-            .and_then(|default_base_path| self.web_resource_providers.0.read().unwrap().get(default_base_path.as_str()).cloned())
+            .and_then(|default_base_path| self.web_resource_providers.0.get(default_base_path.as_str()).map(|p| p.value().clone()))
     }
 
     fn get_default_base_path(&self) -> Option<String> {
         self.default_base_path.read().unwrap().clone()
     }
 
-    fn add_provider(&self, web_resource_provider: Arc<dyn WebResourceProvider>) {
+    fn add_provider(&self, id: Uuid, web_resource_provider: Arc<dyn WebResourceProvider>) {
         let base_path: String = web_resource_provider.get_base_path();
         debug!("Registering web resource provider with base path: {}", base_path);
-        self.web_resource_providers.0.write().unwrap().insert(base_path, web_resource_provider.clone());
+        self.web_resource_providers.0.insert(base_path.clone(), web_resource_provider.clone());
+        self.web_resource_provider_paths.0.insert(id, base_path);
+    }
+
+    fn remove_provider(&self, id: &Uuid) {
+        if let Some(base_path) = self.web_resource_provider_paths.0.get(id) {
+            self.web_resource_providers.0.remove(base_path.value());
+        }
+        self.web_resource_provider_paths.0.remove(id);
     }
 }
 
@@ -65,7 +82,7 @@ impl Lifecycle for WebResourceManagerImpl {
     }
 
     fn shutdown(&self) {
-        // TODO: remove?
-        self.web_resource_providers.0.write().unwrap().clear();
+        self.web_resource_provider_paths.0.clear();
+        self.web_resource_providers.0.clear();
     }
 }
