573d31f925cd5531801e106c836cdd85843e530a
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-12 21:33:21 +0100

    Added constructors and added from trait for NamespacedType

--- src/component_behaviour_type_id.rs
+++ src/component_behaviour_type_id.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 
 use crate::BehaviourTypeId;
 use crate::ComponentTypeId;
+use crate::NamespacedType;
 
 /// The behaviour of a component.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
@@ -12,4 +13,21 @@ pub struct ComponentBehaviourTypeId {
 
     /// The behaviour type.
     pub behaviour_ty: BehaviourTypeId,
+}
+
+impl ComponentBehaviourTypeId {
+    pub fn new(component_ty: ComponentTypeId, behaviour_ty: BehaviourTypeId) -> Self {
+        ComponentBehaviourTypeId { component_ty, behaviour_ty }
+    }
+
+    pub fn new_from_type<S: Into<String>>(namespace: S, type_name: S) -> Self {
+        let namespaced_type = NamespacedType::new(namespace, type_name);
+        ComponentBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
+}
+
+impl From<NamespacedType> for ComponentBehaviourTypeId {
+    fn from(namespaced_type: NamespacedType) -> Self {
+        ComponentBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
 }
