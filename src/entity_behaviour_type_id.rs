7767479243897ea91549656c97c829f8d4de0abf
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-12 21:33:34 +0100

    Added constructors and added from trait for NamespacedType

--- src/entity_behaviour_type_id.rs
+++ src/entity_behaviour_type_id.rs
@@ -3,6 +3,7 @@ use serde::Serialize;
 
 use crate::BehaviourTypeId;
 use crate::EntityTypeId;
+use crate::NamespacedType;
 
 /// The behaviour of an entity type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, Hash)]
@@ -12,4 +13,21 @@ pub struct EntityBehaviourTypeId {
 
     /// The behaviour type.
     pub behaviour_ty: BehaviourTypeId,
+}
+
+impl EntityBehaviourTypeId {
+    pub fn new(entity_ty: EntityTypeId, behaviour_ty: BehaviourTypeId) -> Self {
+        EntityBehaviourTypeId { entity_ty, behaviour_ty }
+    }
+
+    pub fn new_from_type<S: Into<String>>(namespace: S, type_name: S) -> Self {
+        let namespaced_type = NamespacedType::new(namespace, type_name);
+        EntityBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
+}
+
+impl From<NamespacedType> for EntityBehaviourTypeId {
+    fn from(namespaced_type: NamespacedType) -> Self {
+        EntityBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
 }
--- src/relation_behaviour_type_id.rs
+++ src/relation_behaviour_type_id.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 use crate::BehaviourTypeId;
+use crate::NamespacedType;
 use crate::RelationTypeId;
 
 /// The behaviour of a relation type.
@@ -12,4 +13,21 @@ pub struct RelationBehaviourTypeId {
 
     /// The behaviour type.
     pub behaviour_ty: BehaviourTypeId,
+}
+
+impl RelationBehaviourTypeId {
+    pub fn new(relation_ty: RelationTypeId, behaviour_ty: BehaviourTypeId) -> Self {
+        RelationBehaviourTypeId { relation_ty, behaviour_ty }
+    }
+
+    pub fn new_from_type<S: Into<String>>(namespace: S, type_name: S) -> Self {
+        let namespaced_type = NamespacedType::new(namespace, type_name);
+        RelationBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
+}
+
+impl From<NamespacedType> for RelationBehaviourTypeId {
+    fn from(namespaced_type: NamespacedType) -> Self {
+        RelationBehaviourTypeId::new(namespaced_type.clone().into(), namespaced_type.into())
+    }
 }
