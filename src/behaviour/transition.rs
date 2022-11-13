cd9fb92b18855d21ea364d07da0dcf8fa74f353e
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-13 22:28:37 +0100

    Macro for creating transitions of a behaviour

--- src/behaviour/transition.rs
+++ src/behaviour/transition.rs
@@ -3,18 +3,53 @@ use crate::BehaviourConnectFailed;
 use crate::BehaviourDisconnectFailed;
 use crate::BehaviourInitializationFailed;
 use crate::BehaviourShutdownFailed;
+use crate::PropertyObserverContainer;
+use crate::PropertyObserverContainerImpl;
 
 #[allow(drop_bounds)]
 pub trait BehaviourTransitions<T: ReactiveInstance>: Drop {
     /// Initializes the behaviour. For example, calculates and propagates the initial value.
-    fn init(&self) -> Result<(), BehaviourInitializationFailed>;
+    fn init(&self) -> Result<(), BehaviourInitializationFailed> {
+        Ok(())
+    }
 
     /// Connects the reactive streams.
-    fn connect(&self) -> Result<(), BehaviourConnectFailed>;
+    fn connect(&self) -> Result<(), BehaviourConnectFailed> {
+        Ok(())
+    }
 
     /// Disconnects the reactive streams.
-    fn disconnect(&self) -> Result<(), BehaviourDisconnectFailed>;
+    fn disconnect(&self) -> Result<(), BehaviourDisconnectFailed> {
+        self.get_property_observers().remove_all_observers();
+        Ok(())
+    }
 
     /// Destructs the behaviour.
-    fn shutdown(&self) -> Result<(), BehaviourShutdownFailed>;
+    fn shutdown(&self) -> Result<(), BehaviourShutdownFailed> {
+        Ok(())
+    }
+
+    fn get_property_observers(&self) -> &PropertyObserverContainerImpl<T>;
+}
+
+#[macro_export]
+macro_rules! behaviour_transitions {
+    ($transitions: ident, $reactive_instance: ty) => {
+        pub struct $transitions {
+            pub property_observers: PropertyObserverContainerImpl<$reactive_instance>,
+        }
+
+        impl $transitions {
+            pub fn new(property_observers: PropertyObserverContainerImpl<$reactive_instance>) -> Self {
+                $transitions { property_observers }
+            }
+        }
+
+        impl Drop for $transitions {
+            fn drop(&mut self) {
+                let _ = self.disconnect();
+                let _ = self.shutdown();
+            }
+        }
+    };
 }
