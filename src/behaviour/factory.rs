503255cec8c480a0d0d94a6672b037c4267efb02
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-13 19:00:03 +0100

    Macro for behaviour factories

--- src/behaviour/factory.rs
+++ src/behaviour/factory.rs
@@ -12,3 +12,37 @@ pub trait BehaviourFactory<T: ReactiveInstance> {
     /// Returns the behaviour type of the behaviour factory.
     fn behaviour_ty(&self) -> &BehaviourTypeId;
 }
+
+#[macro_export]
+macro_rules! behaviour_factory {
+    ($factory: ident, $behaviour: ty, $reactive_instance: ty) => {
+        pub struct $factory {
+            pub ty: BehaviourTypeId,
+        }
+
+        impl $factory {
+            pub fn new(ty: BehaviourTypeId) -> Self {
+                $factory { ty }
+            }
+        }
+
+        impl BehaviourFactory<$reactive_instance> for $factory {
+            fn create(
+                &self,
+                reactive_instance: Arc<$reactive_instance>,
+            ) -> Result<Arc<dyn BehaviourFsm<$reactive_instance> + Send + Sync>, BehaviourCreationError> {
+                match <$behaviour>::new(reactive_instance, self.ty.clone()) {
+                    Ok(state) => {
+                        let state = state as Arc<dyn BehaviourFsm<$reactive_instance> + Send + Sync>;
+                        Ok(state)
+                    }
+                    Err(e) => Err(e),
+                }
+            }
+
+            fn behaviour_ty(&self) -> &BehaviourTypeId {
+                &self.ty
+            }
+        }
+    };
+}
