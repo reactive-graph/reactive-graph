d013273be0e71b27c8d4ed81c2b760d5211597e5
Andreas Schaeffer <andreas@schaeffernet.de>
2022-11-26 14:39:31 +0100

    Added documentation for the behaviour states

--- src/behaviour/state.rs
+++ src/behaviour/state.rs
@@ -1,8 +1,15 @@
 /// The state of a behaviour.
 #[derive(Debug, Clone, Copy)]
 pub enum BehaviourState {
+    /// The behaviour has been created.
     Created,
+
+    /// The behaviour has been validated.
     Valid,
+
+    /// The behaviour is initialized but not connected.
     Ready,
+
+    /// The behaviour is connected.
     Connected,
 }
