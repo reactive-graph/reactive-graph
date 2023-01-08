0cffd2e0dc629d4116dbee900fd4b8d7ec5c9489
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-08 19:54:23 +0100

    Add namespaced extension type id

--- src/type_id_type.rs
+++ src/type_id_type.rs
@@ -14,6 +14,9 @@ pub const TYPE_ID_TYPE_COMPONENT: &str = "c";
 /// String representation of the type of entity types.
 pub const TYPE_ID_TYPE_ENTITY_TYPE: &str = "e";
 
+/// String representation of the type of extensions.
+pub const TYPE_ID_TYPE_EXTENSION: &str = "x";
+
 /// String representation of the type of relation types.
 pub const TYPE_ID_TYPE_RELATION_TYPE: &str = "r";
 
@@ -23,6 +26,7 @@ pub const TYPE_ID_TYPE_FLOW_TYPE: &str = "f";
 pub static TYPE_ID_TYPE_NAMESPACE_BEHAVIOUR: Uuid = Uuid::from_u128(0x12b7c8109d3d13c180f86c262ff540d9);
 pub static TYPE_ID_TYPE_NAMESPACE_COMPONENT: Uuid = Uuid::from_u128(0x1ab7c8109d3d13c180f468262fd540d9);
 pub static TYPE_ID_TYPE_NAMESPACE_ENTITY_TYPE: Uuid = Uuid::from_u128(0x6ba7c8109dcd11c180b400d04fd530c7);
+pub static TYPE_ID_TYPE_NAMESPACE_EXTENSION: Uuid = Uuid::from_u128(0x6ba7c8109dcd11f586b708d07fd530c7);
 pub static TYPE_ID_TYPE_NAMESPACE_RELATION_TYPE: Uuid = Uuid::from_u128(0x1ab7c8109dcd11c180b400d01fd530c7);
 pub static TYPE_ID_TYPE_NAMESPACE_FLOW_TYPE: Uuid = Uuid::from_u128(0x62b7c5106d3d18c189f468202fd45230);
 
@@ -32,6 +36,7 @@ pub enum TypeIdType {
     Behaviour,
     Component,
     EntityType,
+    Extension,
     RelationType,
     FlowType,
 }
@@ -43,6 +48,7 @@ impl From<TypeIdType> for Uuid {
             TypeIdType::Behaviour => TYPE_ID_TYPE_NAMESPACE_BEHAVIOUR,
             TypeIdType::Component => TYPE_ID_TYPE_NAMESPACE_COMPONENT,
             TypeIdType::EntityType => TYPE_ID_TYPE_NAMESPACE_ENTITY_TYPE,
+            TypeIdType::Extension => TYPE_ID_TYPE_NAMESPACE_EXTENSION,
             TypeIdType::RelationType => TYPE_ID_TYPE_NAMESPACE_RELATION_TYPE,
             TypeIdType::FlowType => TYPE_ID_TYPE_NAMESPACE_FLOW_TYPE,
         }
@@ -56,6 +62,7 @@ impl ToString for TypeIdType {
             TypeIdType::Behaviour => TYPE_ID_TYPE_BEHAVIOUR.to_string(),
             TypeIdType::Component => TYPE_ID_TYPE_COMPONENT.to_string(),
             TypeIdType::EntityType => TYPE_ID_TYPE_ENTITY_TYPE.to_string(),
+            TypeIdType::Extension => TYPE_ID_TYPE_EXTENSION.to_string(),
             TypeIdType::RelationType => TYPE_ID_TYPE_RELATION_TYPE.to_string(),
             TypeIdType::FlowType => TYPE_ID_TYPE_FLOW_TYPE.to_string(),
         }
@@ -70,6 +77,7 @@ impl TryFrom<&str> for TypeIdType {
             TYPE_ID_TYPE_BEHAVIOUR => Ok(TypeIdType::Behaviour),
             TYPE_ID_TYPE_COMPONENT => Ok(TypeIdType::Component),
             TYPE_ID_TYPE_ENTITY_TYPE => Ok(TypeIdType::EntityType),
+            TYPE_ID_TYPE_EXTENSION => Ok(TypeIdType::Extension),
             TYPE_ID_TYPE_RELATION_TYPE => Ok(TypeIdType::RelationType),
             TYPE_ID_TYPE_FLOW_TYPE => Ok(TypeIdType::FlowType),
             _ => Err(()),
