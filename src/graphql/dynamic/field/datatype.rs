92ec2cd9d076f664781aad3fd826781bf29c7cf6
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-14 23:00:52 +0100

    Map property data types to GraphQL type refs

--- src/graphql/dynamic/field/datatype.rs
+++ src/graphql/dynamic/field/datatype.rs
@@ -1,19 +1,45 @@
 use async_graphql::dynamic::TypeRef;
 
 use crate::model::DataType;
+use crate::model::Mutability::Immutable;
+use crate::model::PropertyType;
 
 /// Maps the data type of a property to the GraphQL type ref.
 pub fn to_type_ref(data_type: &DataType) -> TypeRef {
     match data_type {
         DataType::Null => TypeRef::named(TypeRef::STRING),
         DataType::Bool => TypeRef::named(TypeRef::BOOLEAN),
-        DataType::Number => TypeRef::named(TypeRef::FLOAT), // TODO TypeRef::NUMBER???
+        DataType::Number => TypeRef::named(TypeRef::FLOAT), // TODO: NUMBER
         DataType::String => TypeRef::named(TypeRef::STRING),
-        // TODO: Scalar JSON
-        DataType::Array => TypeRef::named_list_nn(TypeRef::STRING),
-        // TODO: Scalar JSON
-        DataType::Object => TypeRef::named(TypeRef::STRING),
-        // TODO: Scalar JSON
-        DataType::Any => TypeRef::named(TypeRef::STRING),
+        // Array = scalar JSON
+        DataType::Array => TypeRef::named("JSON"),
+        // Object = scalar JSON
+        DataType::Object => TypeRef::named("JSON"),
+        // Any = any data type
+        DataType::Any => TypeRef::named("JSON"),
+    }
+}
+
+pub fn to_input_type_ref(property: &PropertyType, is_optional: bool) -> Option<TypeRef> {
+    if is_optional || property.mutability == Immutable {
+        match property.data_type {
+            DataType::Null => None,
+            DataType::Bool => Some(TypeRef::named(TypeRef::BOOLEAN)),
+            DataType::Number => Some(TypeRef::named(TypeRef::FLOAT)), // TODO: "Numeric" (oneof)
+            DataType::String => Some(TypeRef::named(TypeRef::STRING)),
+            DataType::Array => Some(TypeRef::named_nn_list("JSON")),
+            DataType::Object => Some(TypeRef::named("JSON")),
+            DataType::Any => Some(TypeRef::named("JSON")),
+        }
+    } else {
+        match property.data_type {
+            DataType::Null => None,
+            DataType::Bool => Some(TypeRef::named_nn(TypeRef::BOOLEAN)),
+            DataType::Number => Some(TypeRef::named_nn(TypeRef::FLOAT)), // TODO: "Numeric" (oneof)
+            DataType::String => Some(TypeRef::named_nn(TypeRef::STRING)),
+            DataType::Array => Some(TypeRef::named_nn_list_nn("JSON")),
+            DataType::Object => Some(TypeRef::named_nn("JSON")),
+            DataType::Any => Some(TypeRef::named_nn("JSON")),
+        }
     }
 }
