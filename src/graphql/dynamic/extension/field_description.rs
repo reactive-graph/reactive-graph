82f4181ca887447220a4ffbbd1367febaffbf73f
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-14 23:05:08 +0100

    Make it possible to overwrite the field names of outbound and inbound relations

---
+++ src/graphql/dynamic/extension/field_description.rs
@@ -0,0 +1,44 @@
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::from_value;
+
+use crate::core_model::EXTENSION_FIELD_DESCRIPTION;
+use crate::model::Extension;
+use crate::model::ExtensionContainer;
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct DynamicGraphFieldDescriptionExtension {
+    #[serde(default)]
+    pub from_outbound_entity_to_relation: Option<String>,
+
+    #[serde(default)]
+    pub from_relation_to_outbound_entity: Option<String>,
+
+    #[serde(default)]
+    pub from_inbound_entity_to_relation: Option<String>,
+
+    #[serde(default)]
+    pub from_relation_to_inbound_entity: Option<String>,
+
+    #[serde(default)]
+    pub from_outbound_entity_to_inbound_entity: Option<String>,
+
+    #[serde(default)]
+    pub from_inbound_entity_to_outbound_entity: Option<String>,
+}
+
+impl From<Extension> for DynamicGraphFieldDescriptionExtension {
+    fn from(extension: Extension) -> Self {
+        match from_value(extension.extension) {
+            Ok(field_descriptions) => field_descriptions,
+            Err(_) => Default::default(),
+        }
+    }
+}
+
+pub fn get_dynamic_graph_field_descriptions(extension_container: &impl ExtensionContainer) -> DynamicGraphFieldDescriptionExtension {
+    match extension_container.get_own_extension(&EXTENSION_FIELD_DESCRIPTION.clone()) {
+        Some(field_descriptions) => field_descriptions.into(),
+        None => Default::default(),
+    }
+}
