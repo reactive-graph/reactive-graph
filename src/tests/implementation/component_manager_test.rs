958de1a8872627f2471992edfde37e5c507836d2
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-15 14:00:40 +0100

    Updated unit test to use extension types

--- src/tests/implementation/component_manager_test.rs
+++ src/tests/implementation/component_manager_test.rs
@@ -5,6 +5,7 @@ use serde_json::json;
 use crate::model::Component;
 use crate::model::ComponentTypeId;
 use crate::model::Extension;
+use crate::model::ExtensionTypeId;
 use crate::model::NamespacedTypeGetter;
 use crate::model::PropertyType;
 use crate::tests::utils::application::init_application;
@@ -19,21 +20,23 @@ fn test_register_component() {
     let description = r_string();
     let property_name = r_string();
     let extension_name = r_string();
-    let ty = ComponentTypeId::new_from_type(namespace.clone(), component_name.clone());
+    let component_ty = ComponentTypeId::new_from_type(namespace.clone(), component_name.clone());
+    let extension_ty = ExtensionTypeId::new_from_type(&namespace, &extension_name);
     let component = Component::new(
-        &ty,
+        &component_ty,
         &description,
         vec![PropertyType::string(&property_name)],
-        vec![Extension::new(extension_name.clone(), json!(""))],
+        vec![Extension::new(extension_ty.clone(), "", json!(""))],
     );
     assert!(component_manager.register(component).is_ok());
-    assert!(component_manager.has(&ty));
+    assert!(component_manager.has(&component_ty));
 
-    let component = component_manager.get(&ty).unwrap();
+    let component = component_manager.get(&component_ty).unwrap();
     assert_eq!(namespace, component.namespace());
     assert_eq!(component_name, component.type_name());
     assert!(component.has_property(property_name.clone()));
     assert!(!component.has_property(r_string()));
+    assert!(component.has_extension(&extension_ty));
 }
 
 #[test]
@@ -61,19 +64,20 @@ fn test_export_import_component() {
     path.push(format!("{}.json", component_name));
     let path = path.into_os_string().into_string().unwrap();
 
-    let ty = ComponentTypeId::new_from_type(&namespace, &component_name);
+    let component_ty = ComponentTypeId::new_from_type(&namespace, &component_name);
+    let extension_ty = ExtensionTypeId::new_from_type(&namespace, &extension_name);
     let result = component_manager.create(
-        &ty,
+        &component_ty,
         &description,
         vec![PropertyType::string(&property_name)],
-        vec![Extension::new(extension_name.as_str(), json!(""))],
+        vec![Extension::new(extension_ty.clone(), "", json!(""))],
     );
     assert!(result.is_ok());
-    component_manager.export(&ty, path.as_str());
-    assert!(component_manager.has(&ty));
-    component_manager.delete(&ty);
-    assert!(!component_manager.has(&ty));
+    component_manager.export(&component_ty, path.as_str());
+    assert!(component_manager.has(&component_ty));
+    component_manager.delete(&component_ty);
+    assert!(!component_manager.has(&component_ty));
     let result = component_manager.import(path.as_str());
     assert!(result.is_ok());
-    assert!(component_manager.has(&ty));
+    assert!(component_manager.has(&component_ty));
 }
