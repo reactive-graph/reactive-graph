23fe1696838f30e909262667717cd326c195ea19
Andreas Schaeffer <andreas@schaeffernet.de>
2023-01-22 12:37:43 +0100

    Move trigger to its new namespace

--- src/core_model/mod.rs
+++ src/core_model/mod.rs
@@ -5,7 +5,7 @@ use crate::model::entity_ty;
 use crate::model::extension_ty;
 
 pub const NAMESPACE_CORE: &str = "core";
-pub const NAMESPACE_LOGICAL: &str = "logical";
+pub const NAMESPACE_TRIGGER: &str = "trigger";
 pub const NAMESPACE_FLOW: &str = "flow";
 pub const NAMESPACE_DYNAMIC_GRAPH: &str = "dynamic_graph";
 
@@ -17,7 +17,7 @@ pub const PROPERTY_EVENT: &str = "event";
 
 component_ty!(COMPONENT_LABELED, NAMESPACE_CORE, COMPONENT_NAME_LABELED, "labeled");
 component_ty!(COMPONENT_EVENT, NAMESPACE_CORE, COMPONENT_NAME_EVENT, "event");
-component_ty!(COMPONENT_ACTION, NAMESPACE_LOGICAL, COMPONENT_NAME_ACTION, "action");
+component_ty!(COMPONENT_ACTION, NAMESPACE_TRIGGER, COMPONENT_NAME_ACTION, "action");
 
 entity_ty!(ENTITY_TYPE_SYSTEM_EVENT, NAMESPACE_CORE, ENTITY_TYPE_NAME_SYSTEM_EVENT, "system_event");
 entity_ty!(ENTITY_TYPE_SHUTDOWN, NAMESPACE_CORE, ENTITY_TYPE_NAME_SHUTDOWN, "shutdown");
