fa9cc2e8c0ac36b6e32516f48fb59d642d38f5ae
Andreas Schaeffer <andreas@schaeffernet.de>
2023-03-19 22:59:42 +0100

    Implement debug and display for DynamicQueryError

--- crates/runtime/src/api/dynamic_graph_query_service.rs
+++ crates/runtime/src/api/dynamic_graph_query_service.rs
@@ -1,11 +1,28 @@
+use std::fmt;
+
+use async_graphql::Response;
 use async_trait::async_trait;
 use serde_json::Error;
 
 use crate::api::Lifecycle;
 
+#[derive(Debug)]
 pub enum DynamicQueryError {
     DynamicSchemaFailure,
     JsonError(Error),
+}
+
+impl fmt::Display for DynamicQueryError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DynamicQueryError::DynamicSchemaFailure => {
+                write!(f, "Failed to generate the dynamic schema")
+            }
+            DynamicQueryError::JsonError(e) => {
+                write!(f, "JsonError {e}")
+            }
+        }
+    }
 }
 
 #[async_trait]
