c325d53c27b713bb83909fb8e17e5aa311e989fe
Andreas Schaeffer <andreas@schaeffernet.de>
2023-04-18 20:28:18 +0200

    Remove wrong extension type id

--- crates/model/runtime/src/extension/entity_type_category.rs
+++ crates/model/runtime/src/extension/entity_type_category.rs
@@ -2,9 +2,3 @@ use crate::model::extension_ty;
 use crate::NAMESPACE_CORE;
 
 extension_ty!(EXTENSION_ENTITY_TYPE_CATEGORY, NAMESPACE_CORE, EXTENSION_NAME_ENTITY_TYPE_CATEGORY, "entity_type_category");
-extension_ty!(
-    EXTENSION_RELATION_TYPE_CATEGORY,
-    NAMESPACE_CORE,
-    EXTENSION_NAME_RELATION_TYPE_CATEGORY,
-    "entity_type_category"
-);
