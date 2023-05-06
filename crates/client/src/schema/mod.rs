29ee06da3bf52c8ef2ed9287b6eb16de48f410c3
Andreas Schaeffer <andreas@schaeffernet.de>
2023-05-06 14:13:00 +0200

    Allow schema::schema and upper case scalar name

--- crates/client/src/schema/mod.rs
+++ crates/client/src/schema/mod.rs
@@ -8,9 +8,11 @@ pub mod property_type;
 pub mod scalar;
 pub mod socket_type;
 
+#[allow(clippy::module_inception)]
 pub mod schema {
     cynic::use_schema!("schema.graphql");
 }
 
+#[allow(clippy::upper_case_acronyms)]
 type JSON = serde_json::Value;
 cynic::impl_scalar!(JSON, schema::JSON);
