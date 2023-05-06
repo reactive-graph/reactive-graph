3af3d95c1507e970b23353bca149df9d7b3fbda7
Andreas Schaeffer <andreas@schaeffernet.de>
2023-05-06 17:58:49 +0200

    Added CLI environment variables

--- crates/standalone/src/cli_args.rs
+++ crates/standalone/src/cli_args.rs
@@ -30,45 +30,45 @@ pub struct CliArguments {
 
     // Instance
     /// The name of the instance.
-    #[arg(short = 'n', long)]
+    #[arg(short = 'n', long, env = "INEXOR_RGF_INSTANCE_NAME")]
     pub(crate) instance_name: Option<String>,
 
     /// The description of the instance.
-    #[arg(short = 'd', long)]
+    #[arg(short = 'd', long, env = "INEXOR_RGF_INSTANCE_DESCRIPTION")]
     pub(crate) instance_description: Option<String>,
 
     // GraphQL Server
     /// The hostname to bind the GraphQL HTTP server.
-    #[arg(long)]
+    #[arg(long, env = "INEXOR_RGF_HOSTNAME")]
     pub(crate) hostname: Option<String>,
 
     /// The port to bind the GraphQL HTTP server.
-    #[arg(long)]
+    #[arg(long, env = "INEXOR_RGF_PORT")]
     pub(crate) port: Option<u16>,
 
     /// If true, HTTPS is enabled.
-    #[arg(long, action = SetTrue)]
+    #[arg(long, action = SetTrue, env = "INEXOR_RGF_SECURE")]
     pub(crate) secure: Option<bool>,
 
     /// Timeout for graceful workers shutdown in seconds.
     /// After receiving a stop signal, workers have this much time to finish serving requests.
     /// Workers still alive after the timeout are force dropped.
     /// By default shutdown timeout sets to 30 seconds.
-    #[arg(long)]
+    #[arg(long, env = "INEXOR_RGF_INSTANCE_SHUTDOWN_TIMEOUT")]
     pub(crate) shutdown_timeout: Option<u64>,
 
     /// The number of workers to start.
     /// The default worker count is the number of physical CPU cores available.
-    #[arg(short = 'w', long)]
+    #[arg(short = 'w', long, env = "INEXOR_RGF_WORKERS")]
     pub(crate) workers: Option<usize>,
 
     /// The default context path which redirects the root context to a web resource provider.
-    #[arg(short = 'c', long)]
+    #[arg(short = 'c', long, env = "INEXOR_RGF_DEFAULT_CONTEXT_PATH")]
     pub(crate) default_context_path: Option<String>,
 
     // Plugins
     /// If true, all plugins will be disabled.
-    #[arg(short = 'x', long, action = SetTrue)]
+    #[arg(short = 'x', long, action = SetTrue, env = "INEXOR_RGF_DISABLE_ALL_PLUGINS")]
     pub(crate) disable_all_plugins: Option<bool>,
 
     /// The list of plugins to disable.
@@ -76,15 +76,15 @@ pub struct CliArguments {
     pub(crate) disabled_plugins: Option<Vec<String>>,
 
     /// If true, hot deployment will be disabled.
-    #[arg(long, action = SetTrue)]
+    #[arg(long, action = SetTrue, env = "INEXOR_RGF_DISABLE_HOT_DEPLOY")]
     pub(crate) disable_hot_deploy: Option<bool>,
 
     /// If true, the runtime does not wait before exiting.
-    #[arg(long, action = SetTrue)]
+    #[arg(long, action = SetTrue, env = "INEXOR_RGF_STOP_IMMEDIATELY")]
     pub(crate) stop_immediately: Option<bool>,
 
     /// If true, logging is disabled completely.
-    #[arg(short = 'q', long, action = SetTrue)]
+    #[arg(short = 'q', long, action = SetTrue, env = "INEXOR_RGF_QUIET")]
     pub(crate) quiet: Option<bool>,
 }
 
