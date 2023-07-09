e267b789616814eb5bbff65a69ae24aac40a0478
Andreas Schaeffer <andreas@schaeffernet.de>
2023-07-09 18:15:48 +0200

    Ignore clippy lint arc_with_non_send_sync for now
    ... but keep an eye on it

--- crates/core/frp/src/lib.rs
+++ crates/core/frp/src/lib.rs
@@ -3,6 +3,11 @@
 #![feature(unboxed_closures)]
 #![feature(register_tool)]
 #![register_tool(tarpaulin)]
+// Added in rust nightly 1.72.0
+// This lint warns when you use Arc with a type that does not implement Send or Sync.
+// Wrapping a type in Arc doesn't add thread safety to the underlying data, so data races could occur when touching the underlying data.
+// TODO: use mutexes or DashMap
+#![allow(clippy::arc_with_non_send_sync)]
 
 /// Fork of bidule
 /// Adds handle_ids for subscribers
