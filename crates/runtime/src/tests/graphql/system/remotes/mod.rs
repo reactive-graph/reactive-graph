e339caed5ddd163fc4ed2ff3b0ce2fec0ae14c58
Andreas Schaeffer <andreas@schaeffernet.de>
2023-07-13 20:16:43 +0200

    Added GraphQL tests for remotes: update_all, fetch_remotes_from_remote and fetch_remotes_from_all_remotes

--- crates/runtime/src/tests/graphql/system/remotes/mod.rs
+++ crates/runtime/src/tests/graphql/system/remotes/mod.rs
@@ -1,8 +1,11 @@
 mod add;
+mod fetch_remotes_from_all_remotes;
+mod fetch_remotes_from_remote;
 mod get_all;
 mod remove;
 mod remove_all;
 mod update;
+mod update_all;
 
 mod util {
     use async_graphql::Variables;
@@ -12,9 +15,11 @@ mod util {
 
     pub fn address_to_vars(address: &InstanceAddress) -> Variables {
         Variables::from_json(json!({
-            "hostname": address.hostname,
-            "port": address.port,
-            "secure": address.secure
+            "address": {
+                "hostname": address.hostname,
+                "port": address.port,
+                "secure": address.secure
+            }
         }))
     }
 }
---
+++ crates/runtime/src/tests/graphql/system/remotes/update_all.rs
@@ -0,0 +1,84 @@
+use async_graphql::Request;
+use std::sync::Arc;
+
+use serde::Deserialize;
+
+use crate::api::GraphQLQueryService;
+use crate::model_runtime::InstanceInfo;
+use crate::runtime::Runtime;
+use crate::runtime::RuntimeBuilder;
+
+#[derive(Deserialize, Debug)]
+struct Mutation {
+    system: MutationSystem,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct MutationSystem {
+    remotes: MutationSystemUpdateAllRemotes,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct MutationSystemUpdateAllRemotes {
+    update_all: Vec<InstanceInfo>,
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_update_all_remotes() {
+    RuntimeBuilder::new()
+        .ignore_config_files()
+        .disable_all_plugins(true)
+        .pick_free_port()
+        .init()
+        .await
+        .post_init()
+        .await
+        .spawn()
+        .await
+        .with_runtime(|runtime: Arc<dyn Runtime>| async move {
+            let query_service = runtime.get_graphql_query_service();
+            let instance_service = runtime.get_instance_service();
+            let remotes_manager = runtime.get_remotes_manager();
+            remotes_manager.remove_all();
+
+            // RT: Expect that no remotes exists
+            assert_eq!(remotes_manager.get_all().len(), 0);
+
+            // RT: Add self as remote
+            let address = instance_service.get_instance_info().address();
+            let instance_info = remotes_manager.add(&address).await.expect("Failed to add remote");
+
+            // RT: Expect that one remote exists
+            assert_eq!(remotes_manager.get_all().len(), 1);
+
+            // GQL: Update all remote
+            let updated_instance_info = mutation_update_all_remotes(&query_service).await;
+
+            // RT: Expect that one remote was updated
+            assert_eq!(updated_instance_info.len(), 1);
+
+            // Expect that the updated remote matches the remote in the remotes manager
+            assert_eq!(remotes_manager.get(&instance_info.address()), Some(instance_info));
+        })
+        .await
+        .stop()
+        .await
+        .pre_shutdown()
+        .await
+        .shutdown()
+        .await;
+}
+
+const MUTATION_UPDATE_ALL_REMOTES: &str = include_str!("../../../../../graphql/system/remotes/update_all.graphql");
+
+async fn mutation_update_all_remotes(query_service: &Arc<dyn GraphQLQueryService>) -> Vec<InstanceInfo> {
+    let request = Request::new(MUTATION_UPDATE_ALL_REMOTES);
+    let response = query_service.execute(request).await;
+    assert!(response.errors.is_empty());
+    let data = response.data.into_json().expect("Failed to get json data from graphql response");
+    println!("{}", data);
+    let data: Mutation = serde_json::from_value(data).expect("Failed to deserialize json into target data model");
+    data.system.remotes.update_all
+}
