9542a60c23fa5b1f1cf4963ee81125eeb83c966b
Andreas Schaeffer <andreas@schaeffernet.de>
2023-07-13 20:12:29 +0200

    GraphQL: Make instance address an input object + added endpoints update_all and fetch_remotes_from_all_remotes

--- crates/runtime/src/graphql/mutation/system/remotes.rs
+++ crates/runtime/src/graphql/mutation/system/remotes.rs
@@ -3,8 +3,8 @@ use std::sync::Arc;
 use async_graphql::*;
 
 use crate::api::RemotesManager;
+use crate::graphql::mutation::InstanceAddressDefinition;
 use crate::graphql::query::GraphQLInstanceInfo;
-use crate::model_runtime::InstanceAddress;
 
 #[derive(Default)]
 pub struct MutationRemotes;
@@ -13,18 +13,16 @@ pub struct MutationRemotes;
 #[Object]
 impl MutationRemotes {
     /// Adds a remote.
-    async fn add(&self, context: &Context<'_>, hostname: String, port: u16, secure: Option<bool>) -> Result<GraphQLInstanceInfo> {
+    async fn add(&self, context: &Context<'_>, address: InstanceAddressDefinition) -> Result<GraphQLInstanceInfo> {
         let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
-        let address = InstanceAddress::new(hostname, port, secure.unwrap_or(false));
-        let instance = remotes_manager.add(&address).await?;
+        let instance = remotes_manager.add(&address.into()).await?;
         Ok(instance.into())
     }
 
     /// Removes a remote.
-    async fn remove(&self, context: &Context<'_>, hostname: String, port: u16, secure: Option<bool>) -> Result<bool> {
+    async fn remove(&self, context: &Context<'_>, address: InstanceAddressDefinition) -> Result<bool> {
         let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
-        let address = InstanceAddress::new(hostname, port, secure.unwrap_or(false));
-        Ok(remotes_manager.remove(&address))
+        Ok(remotes_manager.remove(&address.into()))
     }
 
     /// Removes all remotes.
@@ -35,20 +33,36 @@ impl MutationRemotes {
     }
 
     /// Updates a remote.
-    async fn update(&self, context: &Context<'_>, hostname: String, port: u16, secure: Option<bool>) -> Result<GraphQLInstanceInfo> {
+    async fn update(&self, context: &Context<'_>, address: InstanceAddressDefinition) -> Result<GraphQLInstanceInfo> {
         let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
-        let address = InstanceAddress::new(hostname, port, secure.unwrap_or(false));
-        let instance = remotes_manager.update(&address).await?;
+        let instance = remotes_manager.update(&address.into()).await?;
         Ok(instance.into())
     }
 
-    /// Fetches the remotes which are available on the given remote.
-    async fn fetch_remotes_from_remote(&self, context: &Context<'_>, hostname: String, port: u16, secure: Option<bool>) -> Result<Vec<GraphQLInstanceInfo>> {
+    /// Updates all remotes.
+    async fn update_all(&self, context: &Context<'_>) -> Result<Vec<GraphQLInstanceInfo>> {
         let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
-        let address = InstanceAddress::new(hostname, port, secure.unwrap_or(false));
+        let updated_remotes = remotes_manager.update_all().await.into_iter().map(|instance| instance.into()).collect();
+        Ok(updated_remotes)
+    }
+
+    /// Fetches the remotes which are available on the given remote.
+    async fn fetch_remotes_from_remote(&self, context: &Context<'_>, address: InstanceAddressDefinition) -> Result<Vec<GraphQLInstanceInfo>> {
+        let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
         let added_instances = remotes_manager
-            .fetch_and_add_remotes_from_remote(&address)
+            .fetch_and_add_remotes_from_remote(&address.into())
             .await?
+            .into_iter()
+            .map(|instance| instance.into())
+            .collect();
+        Ok(added_instances)
+    }
+
+    async fn fetch_remotes_from_all_remotes(&self, context: &Context<'_>) -> Result<Vec<GraphQLInstanceInfo>> {
+        let remotes_manager = context.data::<Arc<dyn RemotesManager>>()?;
+        let added_instances = remotes_manager
+            .fetch_and_add_remotes_from_all_remotes()
+            .await
             .into_iter()
             .map(|instance| instance.into())
             .collect();
---
+++ crates/runtime/src/graphql/mutation/types/instance_address.rs
@@ -0,0 +1,24 @@
+use async_graphql::InputObject;
+use serde::Deserialize;
+use serde::Serialize;
+
+use crate::model_runtime::InstanceAddress;
+
+#[derive(Clone, Debug, Serialize, Deserialize, InputObject)]
+#[graphql(name = "InstanceAddress")]
+pub struct InstanceAddressDefinition {
+    /// The hostname.
+    pub hostname: String,
+
+    /// The port.
+    pub port: u16,
+
+    /// Secure endpoint.
+    pub secure: Option<bool>,
+}
+
+impl From<InstanceAddressDefinition> for InstanceAddress {
+    fn from(address: InstanceAddressDefinition) -> Self {
+        InstanceAddress::new(address.hostname, address.port, address.secure.unwrap_or(false))
+    }
+}
