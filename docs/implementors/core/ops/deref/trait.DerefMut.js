a0530d4339ac244fd9d023c54a33d4ef4c485fed
aschaeffer <aschaeffer@users.noreply.github.com>
2023-07-15 23:17:43 +0000

    deploy: 0a39b6de5554f3ad75744bd1c597d267c4e24563

---
+++ docs/implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"inexor_rgf_core_config":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"inexor_rgf_core_config/remotes/struct.RemotesConfig.html\" title=\"struct inexor_rgf_core_config::remotes::RemotesConfig\">RemotesConfig</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
