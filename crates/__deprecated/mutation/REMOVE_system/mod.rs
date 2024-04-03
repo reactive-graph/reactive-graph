a2d24c97cb159be6023357e4bda120d72a7ac5f8
Andreas Schaeffer <andreas@schaeffernet.de>
2024-04-03 21:50:26 +0200

    Cleanup

--- crates/__deprecated/mutation/REMOVE_system/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use command::*;
-pub use plugin::*;
-pub use remotes::*;
-pub use system::*;
-
-pub mod command;
-pub mod plugin;
-pub mod remotes;
-pub mod system;
--- crates/__deprecated/query/REMOVE_system/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use command::*;
-pub use instance::*;
-pub use plugin::*;
-pub use system::*;
-
-pub mod command;
-pub mod instance;
-pub mod plugin;
-pub mod system;
