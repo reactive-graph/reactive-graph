fc9fce75babc9718ffe67248ca1606647211e2c8
aschaeffer <aschaeffer@users.noreply.github.com>
2024-04-03 19:54:12 +0000

    deploy: a2d24c97cb159be6023357e4bda120d72a7ac5f8

--- docs/trait.impl/inexor_rgf_graphql_api/graphql_query_service/trait.GraphQLQueryService.js
+++ docs/trait.impl/inexor_rgf_graphql_api/graphql_query_service/trait.GraphQLQueryService.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"inexor_rgf_graphql_impl":[["impl <a class=\"trait\" href=\"inexor_rgf_graphql_api/graphql_query_service/trait.GraphQLQueryService.html\" title=\"trait inexor_rgf_graphql_api::graphql_query_service::GraphQLQueryService\">GraphQLQueryService</a> for <a class=\"struct\" href=\"inexor_rgf_graphql_impl/graphql_query_service_impl/struct.GraphQLQueryServiceImpl.html\" title=\"struct inexor_rgf_graphql_impl::graphql_query_service_impl::GraphQLQueryServiceImpl\">GraphQLQueryServiceImpl</a>"]]
+"inexor_rgf_graphql_impl":[["impl GraphQLQueryService for <a class=\"struct\" href=\"inexor_rgf_graphql_impl/graphql_query_service_impl/struct.GraphQLQueryServiceImpl.html\" title=\"struct inexor_rgf_graphql_impl::graphql_query_service_impl::GraphQLQueryServiceImpl\">GraphQLQueryServiceImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
