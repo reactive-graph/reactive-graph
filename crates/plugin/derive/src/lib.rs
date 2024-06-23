a3355c715a06a77d8f00b87fa0f8850f7e765c7c
Andreas Schaeffer <andreas@schaeffernet.de>
2024-06-23 15:06:55 +0200

    The feature `associated_type_bounds` has been stabilized

--- crates/plugin/derive/src/lib.rs
+++ crates/plugin/derive/src/lib.rs
@@ -1,5 +1,3 @@
-// #![feature(associated_type_bounds)]
-//
 // #[macro_use]
 // extern crate darling;
 // extern crate proc_macro;
--- crates/reactive/derive/src/lib.rs
+++ crates/reactive/derive/src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(associated_type_bounds)]
-
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
