976a18840168048ff63050599af85b1113a02a13
Andreas Schaeffer <andreas@schaeffernet.de>
2024-08-10 19:48:34 +0200

    Implemented command to remove a property from a component (client + CLI) + Streamlined CLI argument to client argument conversion

---
+++ crates/client/src/client/types/components/remove_property.rs
@@ -0,0 +1,51 @@
+#[cynic::schema_for_derives(file = r#"schema_graphql.graphql"#, module = "crate::schema_graphql::schema")]
+pub mod queries {
+    use cynic::Operation;
+    use cynic::QueryFragment;
+    use cynic::QueryVariables;
+    use typed_builder::TypedBuilder;
+
+    use crate::schema_graphql::types::component::Component;
+    use reactive_graph_graph::NamespacedTypeGetter;
+
+    #[derive(QueryVariables, Debug, TypedBuilder)]
+    pub struct RemovePropertyVariables {
+        pub namespace: String,
+        pub name: String,
+        pub property_name: String,
+    }
+
+    #[derive(cynic::QueryFragment, Debug)]
+    #[cynic(graphql_type = "Mutation", variables = "RemovePropertyVariables")]
+    pub struct RemoveProperty {
+        pub types: MutationTypes,
+    }
+
+    #[derive(QueryFragment, Debug)]
+    #[cynic(variables = "RemovePropertyVariables")]
+    pub struct MutationTypes {
+        pub components: MutationComponents,
+    }
+
+    #[derive(QueryFragment, Debug)]
+    #[cynic(variables = "RemovePropertyVariables")]
+    pub struct MutationComponents {
+        #[arguments(type: { name: $name, namespace: $namespace }, propertyName: $property_name)]
+        pub remove_property: Component,
+    }
+
+    pub fn remove_property_mutation(ty: reactive_graph_graph::ComponentTypeId, property_name: String) -> Operation<RemoveProperty, RemovePropertyVariables> {
+        use cynic::MutationBuilder;
+        let vars = RemovePropertyVariables {
+            namespace: ty.namespace(),
+            name: ty.type_name(),
+            property_name,
+        };
+        RemoveProperty::build(vars)
+    }
+
+    pub fn remove_property_with_variables(variables: RemovePropertyVariables) -> Operation<RemoveProperty, RemovePropertyVariables> {
+        use cynic::MutationBuilder;
+        RemoveProperty::build(variables)
+    }
+}
