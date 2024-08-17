15c79585bbb7914bce8567eed0ee042dd694ba1a
Andreas Schaeffer <andreas@schaeffernet.de>
2024-08-17 14:32:29 +0200

    Fix clippy lint: to_string_in_format_args

--- crates/reactive-graph/src/table_model/container.rs
+++ crates/reactive-graph/src/table_model/container.rs
@@ -50,6 +50,6 @@ impl<S, T: Clone + Tabled + From<S>, O: TableOptions> From<S> for DefaultTableCo
 
 impl<S, T: Clone + Tabled + From<S>, O: TableOptions> Display for DefaultTableContainer<S, T, O> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.table().to_string())
+        write!(f, "{}", self.table())
     }
 }
