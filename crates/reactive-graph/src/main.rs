f6395be9db59c327a83769f0a1095db1fa53b589
Andreas Schaeffer <andreas@schaeffernet.de>
2024-09-18 22:43:35 +0200

    Added option to run the server process as daemon (closes #152 )

--- crates/reactive-graph/src/main.rs
+++ crates/reactive-graph/src/main.rs
@@ -17,6 +17,17 @@ use crate::server::cli_args::Commands;
 #[cfg(feature = "server")]
 use crate::server::server;
 
+#[cfg(target_os = "linux")]
+use daemonize_me::Daemon;
+#[cfg(target_os = "linux")]
+use daemonize_me::Group;
+#[cfg(target_os = "linux")]
+use daemonize_me::User;
+#[cfg(target_os = "linux")]
+use std::any::Any;
+#[cfg(target_os = "linux")]
+use std::fs::File;
+
 #[cfg(feature = "client")]
 mod cli;
 #[cfg(feature = "server")]
@@ -25,11 +36,68 @@ mod server;
 #[global_allocator]
 static ALLOCATOR: System = System;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     #[cfg(feature = "server")]
     {}
     let cli_args = CliArguments::parse();
+
+    // Export CLI help as markdown
+    if cli_args.markdown_help {
+        clap_markdown::print_help_markdown::<CliArguments>();
+        exit(0);
+    }
+
+    // Initialize daemon
+    #[cfg(target_os = "linux")]
+    {
+        if cli_args.daemon {
+            let daemon = Daemon::new()
+                .work_dir(cli_args.daemon_working_directory.clone().unwrap_or(String::from(".")))
+                .setup_post_fork_parent_hook(post_fork_parent)
+                .setup_post_fork_child_hook(post_fork_child)
+                .setup_post_init_hook(after_init, None);
+            let daemon = match cli_args.daemon_name {
+                Some(ref daemon_name) => daemon.name(daemon_name.as_ref()),
+                None => daemon,
+            };
+            let daemon = match cli_args.daemon_pid {
+                Some(ref daemon_pid) => daemon.pid_file(daemon_pid, Some(false)),
+                None => daemon,
+            };
+            let daemon = match cli_args.daemon_stdout {
+                Some(ref daemon_stdout) => match File::create(daemon_stdout) {
+                    Ok(stdout) => daemon.stdout(stdout),
+                    Err(_) => daemon,
+                },
+                None => daemon,
+            };
+            let daemon = match cli_args.daemon_stderr {
+                Some(ref daemon_stderr) => match File::create(daemon_stderr) {
+                    Ok(stderr) => daemon.stderr(stderr),
+                    Err(_) => daemon,
+                },
+                None => daemon,
+            };
+            let daemon = if let (Some(ref daemon_user), Some(ref daemon_group)) = (cli_args.daemon_user.clone(), cli_args.daemon_group.clone()) {
+                if let (Ok(daemon_user), Ok(daemon_group)) = (User::try_from(daemon_user), Group::try_from(daemon_group)) {
+                    daemon.user(daemon_user).group(daemon_group)
+                } else {
+                    daemon
+                }
+            } else {
+                daemon
+            };
+
+            let daemon = daemon.start();
+            match daemon {
+                Ok(_) => println!("Process has been daemonized with success"),
+                Err(e) => {
+                    eprintln!("Failed to run as daemon: {}", e);
+                    exit(-1);
+                }
+            }
+        }
+    }
 
     // Initialize logging
     if !cli_args.quiet.unwrap_or(false) {
@@ -50,15 +118,41 @@ async fn main() {
             }
         }
     }
-    if cli_args.markdown_help {
-        clap_markdown::print_help_markdown::<CliArguments>();
-        exit(0);
-    }
+
+    tokio_main(cli_args)
+}
+
+#[tokio::main]
+async fn tokio_main(cli_args: CliArguments) {
     match cli_args.commands {
         Some(commands) => match commands {
             #[cfg(feature = "client")]
             Commands::Client(args) => client(args).await,
         },
-        None => server(cli_args).await,
+        None => {
+            #[cfg(feature = "server")]
+            {
+                server(cli_args).await
+            }
+        }
     }
+}
+
+#[cfg(target_os = "linux")]
+fn post_fork_parent(_parent_pid: i32, _child_pid: i32) -> ! {
+    // println!("Exiting parent process. Parent PID: {}, Child PID {}", parent_pid, child_pid);
+    exit(0);
+}
+
+#[cfg(target_os = "linux")]
+fn post_fork_child(_parent_pid: i32, _child_pid: i32) {
+    // println!("Forked into child process. Parent PID: {}, Child PID {}", parent_pid, child_pid);
+    // Child hook must return
+    return;
+}
+
+#[cfg(target_os = "linux")]
+fn after_init(_: Option<&dyn Any>) {
+    // println!("Daemon has been initialized");
+    return;
 }
