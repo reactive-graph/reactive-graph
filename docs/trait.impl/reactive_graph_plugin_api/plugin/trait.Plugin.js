f8ff0cfe952e268e950bdb2040a043caeac0a3b0
aschaeffer <aschaeffer@users.noreply.github.com>
2024-10-18 17:45:27 +0000

    deploy: cab062a14786160c18f499a85d53751b1cf28233

--- docs/trait.impl/reactive_graph_plugin_api/plugin/trait.Plugin.js
+++ docs/trait.impl/reactive_graph_plugin_api/plugin/trait.Plugin.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reactive_graph_plugin_service_impl":[["impl <a class=\"trait\" href=\"reactive_graph_plugin_api/plugin/trait.Plugin.html\" title=\"trait reactive_graph_plugin_api::plugin::Plugin\">Plugin</a> for <a class=\"struct\" href=\"reactive_graph_plugin_service_impl/proxy/struct.PluginProxy.html\" title=\"struct reactive_graph_plugin_service_impl::proxy::PluginProxy\">PluginProxy</a>"]]
+"reactive_graph_plugin_service_impl":[["impl Plugin for <a class=\"struct\" href=\"reactive_graph_plugin_service_impl/proxy/struct.PluginProxy.html\" title=\"struct reactive_graph_plugin_service_impl::proxy::PluginProxy\">PluginProxy</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/trait.impl/reactive_graph_plugin_api/plugin_context/trait.PluginContext.js
+++ docs/trait.impl/reactive_graph_plugin_api/plugin_context/trait.PluginContext.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"reactive_graph_plugin_service_impl":[["impl <a class=\"trait\" href=\"reactive_graph_plugin_api/plugin_context/trait.PluginContext.html\" title=\"trait reactive_graph_plugin_api::plugin_context::PluginContext\">PluginContext</a> for <a class=\"struct\" href=\"reactive_graph_plugin_service_impl/context/struct.PluginContextImpl.html\" title=\"struct reactive_graph_plugin_service_impl::context::PluginContextImpl\">PluginContextImpl</a>"]]
+"reactive_graph_plugin_service_impl":[["impl PluginContext for <a class=\"struct\" href=\"reactive_graph_plugin_service_impl/context/struct.PluginContextImpl.html\" title=\"struct reactive_graph_plugin_service_impl::context::PluginContextImpl\">PluginContextImpl</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
