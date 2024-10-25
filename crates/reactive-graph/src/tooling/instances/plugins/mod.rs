7a592fd14d701ee04909c082928f59d705789bf4
Andreas Schaeffer <andreas@schaeffernet.de>
2024-10-25 20:35:25 +0200

    Use non-owned parameter

--- crates/reactive-graph/src/tooling/instances/plugins/mod.rs
+++ crates/reactive-graph/src/tooling/instances/plugins/mod.rs
@@ -3,14 +3,14 @@ use crate::tooling::instances::plugins::commands::PluginsCommands;
 use crate::tooling::instances::plugins::install::install_plugin;
 use crate::tooling::instances::plugins::uninstall::uninstall_plugin;
 use anyhow::Result;
-use std::path::PathBuf;
+use std::path::Path;
 
 pub mod args;
 pub mod commands;
 pub mod install;
 pub mod uninstall;
 
-pub fn handle_plugins(instance_dir: &PathBuf, args: PluginsArgs) -> Result<()> {
+pub fn handle_plugins(instance_dir: &Path, args: PluginsArgs) -> Result<()> {
     match args.commands {
         PluginsCommands::Install(args) => install_plugin(instance_dir, args)?,
         PluginsCommands::Uninstall(args) => uninstall_plugin(instance_dir, args)?,
--- crates/reactive-graph/src/tooling/instances/repositories/mod.rs
+++ crates/reactive-graph/src/tooling/instances/repositories/mod.rs
@@ -3,14 +3,14 @@ use crate::tooling::instances::repositories::commands::RepositoriesCommands;
 use crate::tooling::instances::repositories::init::init_repository;
 use crate::tooling::instances::repositories::remove::remove_repository;
 use anyhow::Result;
-use std::path::PathBuf;
+use std::path::Path;
 
 pub mod args;
 pub mod commands;
 pub mod init;
 pub mod remove;
 
-pub fn handle_repository(instance_dir: &PathBuf, args: RepositoriesArgs) -> Result<()> {
+pub fn handle_repository(instance_dir: &Path, args: RepositoriesArgs) -> Result<()> {
     match args.commands {
         RepositoriesCommands::Init(args) => init_repository(instance_dir, args)?,
         RepositoriesCommands::Remove(args) => remove_repository(instance_dir, args)?,
