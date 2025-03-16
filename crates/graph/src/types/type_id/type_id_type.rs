04cb5b7563ed449340f256fc90730948bd438155
Andreas Schaeffer <andreas@schaeffernet.de>
2025-03-16 16:16:39 +0100

    Fix unit tests

--- crates/graph/src/types/type_id/type_id_type.rs
+++ crates/graph/src/types/type_id/type_id_type.rs
@@ -116,6 +116,7 @@ mod tests {
     use crate::TYPE_ID_TYPE_NAMESPACE_RELATION_TYPE;
     use crate::TypeIdType;
     use schemars::schema_for;
+    use uuid::Uuid;
 
     #[test]
     fn type_id_type_to_string_test() {
@@ -140,22 +141,22 @@ mod tests {
 
     #[test]
     fn type_id_type_uuid_test() {
-        let tidt_b_uuid = TypeIdType::Behaviour.into();
+        let tidt_b_uuid: Uuid = TypeIdType::Behaviour.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_BEHAVIOUR, tidt_b_uuid);
 
-        let tidt_c_uuid = TypeIdType::Component.into();
+        let tidt_c_uuid: Uuid = TypeIdType::Component.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_COMPONENT, tidt_c_uuid);
 
-        let tidt_e_uuid = TypeIdType::EntityType.into();
+        let tidt_e_uuid: Uuid = TypeIdType::EntityType.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_ENTITY_TYPE, tidt_e_uuid);
 
-        let tidt_x_uuid = TypeIdType::Extension.into();
+        let tidt_x_uuid: Uuid = TypeIdType::Extension.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_EXTENSION, tidt_x_uuid);
 
-        let tidt_r_uuid = TypeIdType::RelationType.into();
+        let tidt_r_uuid: Uuid = TypeIdType::RelationType.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_RELATION_TYPE, tidt_r_uuid);
 
-        let tidt_f_uuid = TypeIdType::FlowType.into();
+        let tidt_f_uuid: Uuid = TypeIdType::FlowType.into();
         assert_eq!(TYPE_ID_TYPE_NAMESPACE_FLOW_TYPE, tidt_f_uuid);
     }
 
