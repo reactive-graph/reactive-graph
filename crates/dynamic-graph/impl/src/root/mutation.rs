0b4d3a85648cb894a38705dcf455d4ca0cb63288
Andreas Schaeffer <andreas@schaeffernet.de>
2025-05-02 23:05:15 +0200

    Sort namespaces in dynamic graph

--- crates/dynamic-graph/impl/src/root/mutation.rs
+++ crates/dynamic-graph/impl/src/root/mutation.rs
@@ -1,12 +1,12 @@
-use async_graphql::dynamic::*;
-
 use crate::namespace_mutation;
 use crate::namespace_mutation_field;
+use async_graphql::dynamic::*;
+use itertools::Itertools;
 use reactive_graph_dynamic_graph_api::SchemaBuilderContext;
 
 pub fn get_mutation(mut schema: SchemaBuilder, context: &SchemaBuilderContext) -> SchemaBuilder {
     let mut mutation = Object::new("Mutation").description("Mutations");
-    for namespace in context.namespace_manager.get_all() {
+    for namespace in context.namespace_manager.get_all().iter().sorted() {
         if let Some(object_namespace) = namespace_mutation(context.clone(), &namespace) {
             mutation = mutation.field(namespace_mutation_field(&namespace));
             schema = schema.register(object_namespace)
