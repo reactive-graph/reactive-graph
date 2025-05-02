0f72c62bb5b3f1ff483d16ad491396f7ddfea85e
Andreas Schaeffer <andreas@schaeffernet.de>
2025-05-02 23:05:15 +0200

    Sort namespaces in dynamic graph

--- crates/dynamic-graph/impl/src/root/query.rs
+++ crates/dynamic-graph/impl/src/root/query.rs
@@ -1,12 +1,12 @@
-use async_graphql::dynamic::*;
-
 use crate::namespace_query;
 use crate::namespace_query_field;
+use async_graphql::dynamic::*;
+use itertools::Itertools;
 use reactive_graph_dynamic_graph_api::SchemaBuilderContext;
 
 pub fn get_query(mut schema: SchemaBuilder, context: &SchemaBuilderContext) -> SchemaBuilder {
     let mut query = Object::new("Query").description("Queries");
-    for namespace in context.namespace_manager.get_all() {
+    for namespace in context.namespace_manager.get_all().iter().sorted() {
         if let Some(object_namespace) = namespace_query(context.clone(), &namespace) {
             query = query.field(namespace_query_field(&namespace));
             schema = schema.register(object_namespace)
