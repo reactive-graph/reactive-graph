1656008b598979e345f4b7a436daa2efa71814f4
Andreas Schaeffer <andreas@schaeffernet.de>
2025-05-17 18:10:42 +0200

    Added script to update the command reference

---
+++ tools/update-cli-command-reference.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cargo +stable build --release
+echo "Generate CLI Command Reference"
+target/release/reactive-graph print-markdown-help > docs/cli/reference/reactive-graph.md
+target/release/reactive-graph-client print-markdown-help > docs/cli/reference/reactive-graph-client.md
+target/release/reactive-graph-server print-markdown-help > docs/cli/reference/reactive-graph-server.md
+target/release/reactive-graph-tooling print-markdown-help > docs/cli/reference/reactive-graph-tooling.md
