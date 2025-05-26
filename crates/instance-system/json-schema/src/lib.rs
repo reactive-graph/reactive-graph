80ac160fc367eda1cb69f9a0fdd9373477e32920
Andreas Schaeffer <andreas.schaeffer@clavisit.com>
2025-05-26 14:53:33 +0200

    SF

--- crates/instance-system/json-schema/src/lib.rs
+++ crates/instance-system/json-schema/src/lib.rs
@@ -3,5 +3,5 @@ pub mod flows;
 pub mod relations;
 
 pub use entities::schema_entity_instances;
-pub use relations::schema_relation_instances;
 pub use flows::schema_flow_instances;
+pub use relations::schema_relation_instances;
