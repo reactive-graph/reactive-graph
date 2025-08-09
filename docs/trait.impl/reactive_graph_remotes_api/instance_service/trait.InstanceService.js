7dc59cf368d090ae17a80d7a18526b7c2721fa43
aschaeffer <aschaeffer@users.noreply.github.com>
2025-08-09 15:45:48 +0000

    deploy: 31eff9bb31f9597d8879e6cc0e602a9c90342f13

--- docs/trait.impl/reactive_graph_remotes_api/instance_service/trait.InstanceService.js
+++ docs/trait.impl/reactive_graph_remotes_api/instance_service/trait.InstanceService.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["reactive_graph_remotes_impl",[["impl <a class=\"trait\" href=\"reactive_graph_remotes_api/instance_service/trait.InstanceService.html\" title=\"trait reactive_graph_remotes_api::instance_service::InstanceService\">InstanceService</a> for <a class=\"struct\" href=\"reactive_graph_remotes_impl/instance_service_impl/struct.InstanceServiceImpl.html\" title=\"struct reactive_graph_remotes_impl::instance_service_impl::InstanceServiceImpl\">InstanceServiceImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["reactive_graph_remotes_impl",[["impl InstanceService for <a class=\"struct\" href=\"reactive_graph_remotes_impl/instance_service_impl/struct.InstanceServiceImpl.html\" title=\"struct reactive_graph_remotes_impl::instance_service_impl::InstanceServiceImpl\">InstanceServiceImpl</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[467]}
+//{"start":57,"fragment_lengths":[286]}
--- docs/trait.impl/reactive_graph_remotes_api/remotes_system/trait.RemotesSystem.js
+++ docs/trait.impl/reactive_graph_remotes_api/remotes_system/trait.RemotesSystem.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["reactive_graph_remotes_impl",[["impl <a class=\"trait\" href=\"reactive_graph_remotes_api/remotes_system/trait.RemotesSystem.html\" title=\"trait reactive_graph_remotes_api::remotes_system::RemotesSystem\">RemotesSystem</a> for <a class=\"struct\" href=\"reactive_graph_remotes_impl/remotes_system_impl/struct.RemotesSystemImpl.html\" title=\"struct reactive_graph_remotes_impl::remotes_system_impl::RemotesSystemImpl\">RemotesSystemImpl</a>"]]],["reactive_graph_runtime_impl",[["impl RemotesSystem for <a class=\"struct\" href=\"reactive_graph_runtime_impl/runtime_impl/struct.RuntimeImpl.html\" title=\"struct reactive_graph_runtime_impl::runtime_impl::RuntimeImpl\">RuntimeImpl</a>"]]]]);
+    var implementors = Object.fromEntries([["reactive_graph_remotes_impl",[["impl RemotesSystem for <a class=\"struct\" href=\"reactive_graph_remotes_impl/remotes_system_impl/struct.RemotesSystemImpl.html\" title=\"struct reactive_graph_remotes_impl::remotes_system_impl::RemotesSystemImpl\">RemotesSystemImpl</a>"]]],["reactive_graph_runtime_impl",[["impl RemotesSystem for <a class=\"struct\" href=\"reactive_graph_runtime_impl/runtime_impl/struct.RuntimeImpl.html\" title=\"struct reactive_graph_runtime_impl::runtime_impl::RuntimeImpl\">RuntimeImpl</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[447,243]}
+//{"start":57,"fragment_lengths":[274,243]}
